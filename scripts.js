66ab0779b895460064306bd1898b1652797fa4ee
marthinv78 <85097500+marthinv78@users.noreply.github.com>
2026-06-07 19:21:22 -0600

    3

--- scripts.js
+++ scripts.js
@@ -1,22 +1,11 @@
 window.addEventListener("scroll", function() {
 
     const header = document.getElementById("header");
-    const backToTop = document.getElementById("backToTop");
 
-    // Menú
     if(window.scrollY > 50){
         header.classList.add("scrolled");
     }else{
         header.classList.remove("scrolled");
-    }
-
-    // Botón volver arriba
-    if(backToTop){
-        if(window.scrollY > 300){
-            backToTop.classList.add("show");
-        }else{
-            backToTop.classList.remove("show");
-        }
     }
 
 });
